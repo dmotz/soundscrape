eb22c6dbad286ed0c6511abdd5c7226e804e7d99
dmotz <motzdc@gmail.com>
2012-03-31 23:42:00 -0400

    test for write permission, smarter parsing

--- soundscrape.coffee
+++ soundscrape.coffee
@@ -1,29 +1,32 @@
 ###
-* soundscrape
-* Dan Motzenbecker
+ * soundscrape
+ * SoundCloud command line downloader
+ * Dan Motzenbecker <motzdc@gmail.com>
+ * MIT License
 ###
+
 
 http = require 'http'
 fs   = require 'fs'
 
 rootHost = 'soundcloud.com'
-page = 1
-argLen = process.argv.length
+page     = 1
+argLen   = process.argv.length
+params   = {}
 
 
 scrape = ->
   http.get
     host: rootHost
-    path: '/' + artist + '/' + (if trackName? then trackName else 'tracks?page=' + page)
+    path: '/' + params.artist + '/' +
+      (if params.trackName? then params.trackName else 'tracks?page=' + page)
   , (res) ->
-    data = ''
-    res.on 'data', (chunk) ->
-      data += chunk
 
+    data = ''
+    res.on 'data', (chunk) -> data += chunk
     res.on 'end', ->
       tracks = data.match /(window\.SC\.bufferTracks\.push\().+(?=\);)/gi
-
-      if trackName?
+      if params.trackName?
         download parse tracks[0]
         console.log ''
       else
@@ -36,39 +39,60 @@ scrape = ->
 
 
 parse = (raw) ->
-  JSON.parse raw.substr 28
+  chaff = raw.indexOf '{'
+  return false if chaff is -1
+  try
+    JSON.parse raw.substr chaff
+  catch e
+    console.log '\x1b[31m' + 'couldn\'t parse this page.' + '\x1b[0m'
+    process.exit 1
 
 
 download = (obj) ->
+  return if !obj
   artist = obj.user.username
-  title = obj.title
-  console.log '\x1b[33mfetching: ' + title + '\x1b[0m'
+  title  = obj.title
+  console.log '\x1b[33m' + 'fetching: ' + title + '\x1b[0m'
   http.get
     host: 'media.' + rootHost
     path: obj.streamUrl.match /\/stream\/.+/
   , (res) ->
+
     res.on 'end', ->
+      host = 'ak-media.' + rootHost
       http.get
-        host: 'ak-media.' + rootHost
-        path: res.headers.location.substr 30
+        host: host
+        path: res.headers.location.substr host.length
       , (res) ->
         file = fs.createWriteStream './' + artist + ' - ' + title + '.mp3'
 
-        res.on 'data', (chunk) ->
-          file.write chunk
-
+        res.on 'data', (chunk) -> file.write chunk
         res.on 'end', ->
           file.end()
-          console.log '\x1b[32mdone:     ' + title + '\x1b[0m'
+          console.log '\x1b[32m' + 'done:     ' + title + '\x1b[0m'
 
 
+init = ->
+  if argLen <= 2
+    console.log '\x1b[31m' + 'pass an artist name!' + '\x1b[0m'
+    process.exit 1
 
-if argLen < 2
-  return console.log 'pass an artist name!'
+  testFile = '.soundscrape_' + Date.now()
+  try
+    writeTest = fs.createWriteStream testFile
+  catch e
+    console.log '\x1b[31m' + 'you don\'t have permission to write files here' + '\x1b[0m'
+    process.exit 1
 
-artist = process.argv[2]
+  writeTest.end()
+  fs.unlink testFile, (err) -> if err? then console.log err
 
-if argLen > 3
-  trackName = process.argv[3]
+  params.artist = process.argv[2]
 
-scrape()
+  if argLen > 3
+    params.trackName = process.argv[3]
+
+  scrape()
+
+
+init()
--- soundscrape.js
+++ soundscrape.js
@@ -1,11 +1,13 @@
 
 /*
-* soundscrape
-* Dan Motzenbecker
+ * soundscrape
+ * SoundCloud command line downloader
+ * Dan Motzenbecker <motzdc@gmail.com>
+ * MIT License
 */
 
 (function() {
-  var argLen, artist, download, fs, http, page, parse, rootHost, scrape, trackName;
+  var argLen, download, fs, http, init, page, params, parse, rootHost, scrape;
 
   http = require('http');
 
@@ -17,10 +19,12 @@
 
   argLen = process.argv.length;
 
+  params = {};
+
   scrape = function() {
     return http.get({
       host: rootHost,
-      path: '/' + artist + '/' + (typeof trackName !== "undefined" && trackName !== null ? trackName : 'tracks?page=' + page)
+      path: '/' + params.artist + '/' + (params.trackName != null ? params.trackName : 'tracks?page=' + page)
     }, function(res) {
       var data;
       data = '';
@@ -30,7 +34,7 @@
       return res.on('end', function() {
         var track, tracks, _i, _len;
         tracks = data.match(/(window\.SC\.bufferTracks\.push\().+(?=\);)/gi);
-        if (typeof trackName !== "undefined" && trackName !== null) {
+        if (params.trackName != null) {
           download(parse(tracks[0]));
           return console.log('');
         } else {
@@ -50,22 +54,33 @@
   };
 
   parse = function(raw) {
-    return JSON.parse(raw.substr(28));
+    var chaff;
+    chaff = raw.indexOf('{');
+    if (chaff === -1) return false;
+    try {
+      return JSON.parse(raw.substr(chaff));
+    } catch (e) {
+      console.log('\x1b[31m' + 'couldn\'t parse this page.' + '\x1b[0m');
+      return process.exit(1);
+    }
   };
 
   download = function(obj) {
     var artist, title;
+    if (!obj) return;
     artist = obj.user.username;
     title = obj.title;
-    console.log('\x1b[33mfetching: ' + title + '\x1b[0m');
+    console.log('\x1b[33m' + 'fetching: ' + title + '\x1b[0m');
     return http.get({
       host: 'media.' + rootHost,
       path: obj.streamUrl.match(/\/stream\/.+/)
     }, function(res) {
       return res.on('end', function() {
+        var host;
+        host = 'ak-media.' + rootHost;
         return http.get({
-          host: 'ak-media.' + rootHost,
-          path: res.headers.location.substr(30)
+          host: host,
+          path: res.headers.location.substr(host.length)
         }, function(res) {
           var file;
           file = fs.createWriteStream('./' + artist + ' - ' + title + '.mp3');
@@ -74,19 +89,35 @@
           });
           return res.on('end', function() {
             file.end();
-            return console.log('\x1b[32mdone:     ' + title + '\x1b[0m');
+            return console.log('\x1b[32m' + 'done:     ' + title + '\x1b[0m');
           });
         });
       });
     });
   };
 
-  if (argLen < 2) return console.log('pass an artist name!');
+  init = function() {
+    var testFile, writeTest;
+    if (argLen <= 2) {
+      console.log('\x1b[31m' + 'pass an artist name!' + '\x1b[0m');
+      process.exit(1);
+    }
+    testFile = '.soundscrape_' + Date.now();
+    try {
+      writeTest = fs.createWriteStream(testFile);
+    } catch (e) {
+      console.log('\x1b[31m' + 'you don\'t have permission to write files here' + '\x1b[0m');
+      process.exit(1);
+    }
+    writeTest.end();
+    fs.unlink(testFile, function(err) {
+      if (err != null) return console.log(err);
+    });
+    params.artist = process.argv[2];
+    if (argLen > 3) params.trackName = process.argv[3];
+    return scrape();
+  };
 
-  artist = process.argv[2];
-
-  if (argLen > 3) trackName = process.argv[3];
-
-  scrape();
+  init();
 
 }).call(this);
